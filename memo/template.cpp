9c706a1641a01f1c3a8923ca93ce74d738d928f0
ferin <ferin@example.com>
2018-10-09 21:33:34 +0900

    fix template

--- memo/template.cpp
+++ memo/template.cpp
@@ -1,39 +1,46 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
 using ll = long long;
-#define int ll
+// #define int ll
 using PII = pair<int, int>;
-template <typename T> using V = vector<T>;
-template <typename T> using VV = vector<V<T>>;
-template <typename T> using VVV = vector<VV<T>>;
-
+ 
 #define FOR(i, a, n) for (ll i = (ll)a; i < (ll)n; ++i)
 #define REP(i, n) FOR(i, 0, n)
 #define ALL(x) x.begin(), x.end()
-#define PB push_back
-
-const ll INF = (1LL<<60);
-const int MOD = 1000000007;
-
-template <typename T> T &chmin(T &a, const T &b) { return a = min(a, b); }
-template <typename T> T &chmax(T &a, const T &b) { return a = max(a, b); }
-template <typename T> bool IN(T a, T b, T x) { return a<=x&&x<b; }
+ 
+template<typename T> T &chmin(T &a, const T &b) { return a = min(a, b); }
+template<typename T> T &chmax(T &a, const T &b) { return a = max(a, b); }
+template<typename T> bool IN(T a, T b, T x) { return a<=x&&x<b; }
 template<typename T> T ceil(T a, T b) { return a/b + !!(a%b); }
+
+template<typename T> vector<T> make_v(size_t a) { return vector<T>(a); }
+template<typename T,typename... Ts>
+auto make_v(size_t a,Ts... ts) { 
+  return vector<decltype(make_v<T>(ts...))>(a,make_v<T>(ts...));
+}
+template<typename T,typename V> typename enable_if<is_class<T>::value==0>::type
+fill_v(T &t, const V &v) { t=v; }
+template<typename T,typename V> typename enable_if<is_class<T>::value!=0>::type
+fill_v(T &t, const V &v ) { for(auto &e:t) fill_v(e,v); }
+
 template<class S,class T>
 ostream &operator <<(ostream& out,const pair<S,T>& a){
-  out<<'('<<a.first<<','<<a.second<<')';
-  return out;
+  out<<'('<<a.first<<','<<a.second<<')'; return out;
+}
+template<typename T>
+istream& operator >> (istream& is, vector<T>& vec){
+  for(T& x: vec) {is >> x;} return is;
 }
 template<class T>
 ostream &operator <<(ostream& out,const vector<T>& a){
-  out<<'[';
-  REP(i, a.size()) {out<<a[i];if(i!=a.size()-1)out<<',';}
-  out<<']';
-  return out;
+  out<<'['; for(T i: a) {out<<i<<',';} out<<']'; return out;
 }
 
-int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0}; // DRUL
+const int INF = 1<<30;
+const ll LLINF = 1LL<<60;
+const int MOD = 1000000007;
 
 signed main(void)
 {
