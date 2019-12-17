f0f11ea46e029f5d49bcc018f966f71574b412dd
ferin <ferin.tech15@gmail.com>
2019-12-17 15:38:10 +0900

    fix SWAg

--- data_structure/sliding_window_aggregation.cpp
+++ data_structure/sliding_window_aggregation.cpp
@@ -1,31 +1,28 @@
-template<class T, class F>
-struct slidingWindowAggregation {
-    // using F = function<T(T,T)>;
-    F f; T d;
-    stack<pair<T,T>> st1, st2;
-    slidingWindowAggregation(F f, T d) : f(f), d(d) {}
-    // f({x_1, x_2, …, x_n}) を求める
-    T fold_all() { 
-        if(st1.size() && st2.size()) return f(st1.top().second, st2.top().second); 
-        if(st2.size()) return st2.top().second;
-        if(st1.size()) return st1.top().second;
-        return d;
+template<class T, class S, class F>
+struct SWAG {
+    // using F = function<S(S,T)>;
+    F f; S id;
+    stack<T> lt, rt;
+    stack<S> ls, rs;
+    SWAG(F f, S d) : f(f), id(d) {
+        ls.push(id);
+        rs.push(id);
     }
-    // 末尾にx追加
-    void push(T x) { 
-        const T rui = st2.size() ? f(st2.top().second, x) : x;
-        st2.push({x, rui}); 
+    void push_back(T x) { 
+        rt.push(x);
+        rs.push(f(rs.top(), x));
     }
-    // 先頭削除
-    void pop() {
-        if(st1.empty()) {
-            T rui = d;
-            while(st2.size()) {
-                T t; tie(t, ignore) = st2.top(); st2.pop();
-                rui = f(rui, t);
-                st1.push({t, rui});
+    void pop_front() {
+        if(lt.empty()) {
+            while(rt.size()) {
+                T x = rt.top(); rt.pop(); rs.pop();
+                lt.push(x);
+                ls.push(f(ls.top(), x));
             }
         }
-        st1.pop();
+        lt.pop();
+        ls.pop();
     }
+    template<class Q>
+    void fold(Q q) { q(ls.top(), rs.top()); }
 };
