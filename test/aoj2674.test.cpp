0aa7367030d37c1fa8e6e5cc3c9f63830fca0d79
ferin-15 <ferin.tech15@gmail.com>
2020-01-21 02:41:10 +0900

    add test aoj2674 for wavelet matrix

---
+++ test/aoj2674.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2674"
+#include "../memo/macro.hpp"
+#include "../data_structure/wavelet_matrix.cpp"
+
+int main() {
+    ll n;
+    cin >> n;
+    array<int, 100000> x;
+    const int offset = 2e8;
+    REP(i, n) cin >> x[i], x[i] += offset;
+    wavelet<int, 100000, 30> wm(n, x);
+
+    ll q;
+    cin >> q;
+    vector<ll> ans(q);
+    REP(i, q) {
+        ll l, r, e;
+        cin >> l >> r >> e;
+        l--, r--;
+        ll a = min(x[l], x[r]), b = max(x[l], x[r]);
+        // cerr << l << " " << r << " " << a-e << " " << b+e << endl;
+        cout << r-l+1 - wm.rangefreq(l, r+1, a-e, b+e+1) << "\n";
+        // ans[i] = r-l+1 - wm.rangefreq(l, r+1, a-e, b+e+1);
+    }
+    // for(auto i: ans) cout << i << "\n";
+    cout << flush;
+}
