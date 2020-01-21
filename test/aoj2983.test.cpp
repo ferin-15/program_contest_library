4da3b533b0f4309307d36d553fcb180d0917d031
ferin-15 <ferin.tech15@gmail.com>
2020-01-21 22:42:00 +0900

    add test for max_matching

--- test/aoj2983.test.cpp
+++ test/aoj2983.test.cpp
@@ -2,19 +2,7 @@
 #include "../memo/macro.hpp"
 #include "../math/runtime_modint.cpp"
 #include "../math/finite_field_matrix.cpp"
-
-ll max_matching(ll n, vector<PII> edges) {
-    mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
-    uniform_int_distribution<ll> dist(1, MOD-1);
-
-    matrix mat(n, n);
-    for(auto e: edges) {
-        ll r = dist(mt);
-        mat.get(e.first, e.second) = r;
-        mat.get(e.second, e.first) = MOD-r;
-    }
-    return (gauss_jordan(mat)+1)/2;
-}
+#include "../graph/max_matching.cpp"
 
 signed main(void) {
     MOD = 1000000007;
