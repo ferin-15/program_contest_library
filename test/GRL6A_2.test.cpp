f877034a23f1ebcce9cdd2c6697277e0eccee628
ferin-15 <ferin.tech15@gmail.com>
2020-01-21 21:34:27 +0900

    add test for dinic cost scalling

---
+++ test/GRL6A_2.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/6/GRL_6_A"
+#include "../memo/macro.hpp"
+#include "../graph/dinic_with_cost_scalling.cpp"
+
+signed main(void) {
+    ll v, e;
+    cin >> v >> e;
+    dinicWithCostScalling<ll> flow(v);
+    REP(i, e) {
+        ll u, v, c;
+        cin >> u >> v >> c;
+        flow.add_edge(u, v, c);
+    }
+
+    cout << flow.maxflow(0, v-1) << endl;
+
+    return 0;
+}
